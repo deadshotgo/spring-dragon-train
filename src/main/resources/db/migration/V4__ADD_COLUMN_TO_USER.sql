3a97225d743b994fcbd17bb8db57c95b529ea555
Deadshotgo <kovalenkoole63@gmail.com>
2024-06-03 01:55:04 +0300

    add auth provider

--- src/main/resources/db/migration/V4__ADD_COLUMN_TO_USER.sql
+++ src/main/resources/db/migration/V4__ADD_COLUMN_TO_USER.sql
@@ -1,3 +1,3 @@
 alter table app_user add column roles VARCHAR(255) default 'ROLE_USER';
 
-update app_user set roles = 'ROLE_ADMIN' where id = 1;
+update app_user set roles = 'ROLE_ADMIN', password = '$2a$10$sAGzQD/I8c36hz884rUvcuEE7dCTR7wBE3EDkQ5.eOjOp5YvFqgPG' where id = 1;
